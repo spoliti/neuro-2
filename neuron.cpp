98e4abca3c4711a8d76290072fa5bb3bd0640af2
camillehyde <hyde@SV-BBCF043.intranet.epfl.ch>
2016-10-10 15:14:13 +0200

    nouvelle version neuron

--- neuron.cpp
+++ neuron.cpp
@@ -3,7 +3,31 @@
 
 //constructeur destructeur?
 
-void Neuron::recoit_spike(){
-	++compteur_spike;   //modifie le compteur de spikes mais je ne sais 
-						//pas encore comment
-}
+
+//methodes
+	
+	Neuron::Neuron(int numero_neuron, double compteur_spikes,
+
+	int Neuron:: get_numero(){
+		return numero_neuron;
+	}
+	
+	double Neuron:: get compteur(){
+		return compteur_spikes;
+	}
+	
+	double Neuron::get_potentiel(){
+		return potentiel;
+	}
+	
+	void Neuron :: reset(){
+		//remet le potentiel du neurone a la valeur v_reset après avoir
+		//envoyé un spike
+		potentiel = v_reset;
+	}
+	
+	void Neuron ::refractory(){
+		//rend le neurone incapable de produire un spike pendant un certain
+		//temps (refractory_time)
+		
+	}
