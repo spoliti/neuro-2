38540404cad21232cc88a630b7fda2903804aad3
camillehyde <hyde@SV-BBCF043.intranet.epfl.ch>
2016-10-10 15:15:19 +0200

    nouvelle version neurone

--- neuron.hpp
+++ neuron.hpp
@@ -1,18 +1,44 @@
-
+#ifndef NEURON_H
+#define NEURON_H
 #include <vector>
 
 class Neuron
 {
 //attributs
-double compteur_spikes;
-vector <Neuron> neurones_voisins(10000); //je ne sais pas s'il est possible 
-										//de faire un tableau de neurones 
-										//à l'intérieur de la classe neuron elle meme
+private:
+	const int numero_neuron;
+	double compteur_spikes;
+	double potentiel;
+	bool state;
+	
+	const static int v_reset(10); //[milliVolt]
+	const static int refractory_period(2); //[milliseconde]
+	const static double connection_probability(0.1);
+	const static int firing_threshold(20); //[milliVolt]
+	const static double transmission_delay(1.5); //[milliseconde]
+	const static int membrane_time(20); //[milliseconde]
+	
+	static double freq_thr(); //frequency needed for a neuron to reach
+								//threshold in absence of feedback
+	double g; //relative strength of inhibitory synapses
+	
+
 
 //methodes
-//constructeur destructeur?
 
-void recoit_spike();
+//constructeur
+	Neuron
+//getters
+	int get_numero();
+	double get compteur();
+	double get_potentiel();
+	
+	
+	void reset();
+	void refractory();
+
+	
+
 };
 
 #endif 
