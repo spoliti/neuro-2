d8a1937811eba969daf62c7085823a8b33823419
bichoff <frederic.bischoff@epfl.ch>
2016-10-17 13:28:54 +0200

    fichiers env1 et neurone ajoutés

---
+++ Neurone.cpp
@@ -0,0 +1,16 @@
+# include "Neurone.hpp"
+# include <iostream>
+using namespace std;
+
+
+
+Neurone::Neurone() {
+	cout << "donner un numero au neuronne" << endl;
+	
+	/*int n;
+	cin >> n;
+	numero = n;
+	cout << "je suis le neurone numero " << numero << endl;
+	*/											
+}
+
---
+++ Neurone.hpp
@@ -0,0 +1,14 @@
+#ifndef NEURON_H
+#define NEURON_H
+
+
+
+class Neurone {
+	public:
+		Neurone();					// le constructeur par défaut
+		//~Neurone();
+		int numero;
+		
+};
+
+#endif
