28b07cb7731ab7c22b10ea8abf9b60e649b47c2c
OliviaCourbot <olivia.courbot@epfl.ch>
2016-10-24 13:38:54 +0200

    ajout main

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include "neuron.hpp"
+#include "Env.cpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Env; 
+
+int main() {
+	
+	//Création de la simulation
+	//Env(); //constructeur de env pour créer des neurones
+	Env network;
+	//appel de random_connection pour générer les connections
+	network.Env::random_connection();
+
+	
+	//Mise en route de la simulation
+	
+	
+	return 0;
+} 
+
