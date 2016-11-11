e38e8850bccf72c53bbfd264c83b85cef25be6a7
OliviaCourbot <olivia.courbot@epfl.ch>
2016-11-11 17:33:34 +0100

    pour test du parametrage

--- parametrage/src/mainparam.cpp
+++ parametrage/src/mainparam.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-#include <tclap/CmdLine.h> //il faut télécharger et installer tclap
+#include "../lib/tclap/include/tclap/CmdLine.h" //il faut télécharger et installer tclap
 using namespace TCLAP; 
 //using namespace std;
 
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
     	cmd.add(ratio_arg); 
     
 	cmd.parse( argc, argv ); 
-									/ est ce que je dois faire une "boucle if"? qu il me change la valeur du temps si et seulement si on a changé la valeur via ValueArg
+									// est ce que je dois faire une "boucle if"? qu il me change la valeur du temps si et seulement si on a changé la valeur via ValueArg
 	network.Env::time_simu=time_simu_arg.getValue();  				// va attribuer la nouvelle valeur choisie pour l'attribut temps
 											/*excitatory_neurons=excitatory_neurons_arg.getValue(); / je peux déclarer le temps dans le main. Mais mes valeurs des neurones sont dans le fichier neurones, mais en plus je veux un tableau de neurones
 											inhibitory_neurons=inhibatory_neurons_arg.getValue();/ qui lui est un Env.il faudrai donc un getters dans les fonctions de Env, pour de nouveau déclarer avant int excitatory_neurons(network.Env::get_neurones())*/
@@ -51,6 +51,8 @@ int main(int argc, char** argv) {
 	
 	network.Env::get_inhibitory_neurons()=network.Env::get_excitatory_neurons*network.Env::g;
 	
+	//ON NE PEUT PAS DECLARER LES VARIABLES ICI CAR PB DE PORTÉE 
+	//IE ON NE PEUT PAS LES UTILISER DANS LE MAIN
 	double ratio ( network.Env::ratio);
 	int excitatory_neurons(network.Env::get_excitatory_neurons());
 	int inhibatory_neurons(network.Env::get_inhibatory_neurons());
@@ -71,10 +73,10 @@ int main(int argc, char** argv) {
 
 
 	//Mise en route de la simulation
+	//int time_simu(network.Env::get_time_simu());
     
     
-    
-    for(int i(0); i <= time_simu; ++i){ /  ceci peut rester exactement comme ceci
+    for(int i(0); i <= time_simu; ++i){ //  ceci peut rester exactement comme ceci
     //for (int i(0); i < time_simu; i += Env::time_unit) {
         
         //lancement des spikes sur des cycles de 10 unités de temps (ms, s ?)
