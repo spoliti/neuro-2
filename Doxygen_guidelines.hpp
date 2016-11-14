67e9bd2f9db118a1c58c2791013852a8d63b781d
Charlotte Lasne <Charlotte@tsf-476-wpa-7-251.epfl.ch>
2016-11-14 12:50:52 +0100

    ajout tutoriel doxygen

---
+++ Doxygen_guidelines.hpp
@@ -0,0 +1,43 @@
+#ifndef FILENAME_H
+#define FILENAME_H
+
+
+
+//To describe a class.
+/*!
+ * @class Name
+ *
+ * @brief DESCRIPTION OF THE CLASS
+ */
+class Name
+ {
+	public:
+	
+	//To describe a variable. 
+	type_attribute attribute; /*!< Brief description  */ //Don't forget the '<' !!
+	
+	
+	
+	//To describe a method.
+	//Describe first what does the method do (briefly), then describe the parameters, and finally describe what your method returns.
+	/*!
+     * @brief DESCRIPTION OF FUNCTION
+     * 
+     * Detailed description starts here.
+     *
+     * @param INPUT 1
+     * @param INPUT 2
+     * 
+     * @return OUTPUT
+     * 
+     * @see If you want to refer to another method, for example antoher method called in your method. 
+     */
+     output_type function(input1, input2);
+     
+     
+ 
+     
+};
+ 
+ 
+ #endif
